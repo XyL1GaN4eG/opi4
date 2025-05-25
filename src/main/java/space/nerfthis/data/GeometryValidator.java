248635a543be64493dd6a5876dd7b7b2eea9f790
XyL1GaN4eG <trolloguy@mail.ru>
2025-05-25 16:57:37 +0300

    refactor

--- src/main/java/space/nerfthis/data/GeometryValidator.java
+++ src/main/java/space/nerfthis/data/GeometryValidator.java
@@ -2,24 +2,14 @@ package space.nerfthis.data;
 
 public class GeometryValidator {
     public static boolean isInsideArea(double x, double y, double r) {
+        if (x <= 0 && y >= 0)
+            return y <= x + (r / 2.0);
 
-        if (x <= 0 && y >= 0) {
-            if (y <= x + (r / 2.0)) {
-                return true;
-            }
-        }
+        if (x >= -r && x <= 0)
+            return y >= -(r / 2.0);
 
-        if (x >= -r && x <= 0) {
-            if (y >= -(r / 2.0)) {
-                return true;
-            }
-        }
-
-        if (x >= 0 && y >= 0) {
-            if (Math.pow(x, 2) + Math.pow(y, 2) <= Math.pow(r, 2)) {
-                return true;
-            }
-        }
+        if (x >= 0 && y >= 0)
+            return Math.pow(x, 2) + Math.pow(y, 2) <= Math.pow(r, 2);
 
         return false;
     }
